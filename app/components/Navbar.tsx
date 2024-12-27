3ad0be8536c228951141ea5be37008840b23908a
gitPirate12 <aneeqshaffy7@gmail.com>
2024-12-27 19:19:17 +0530

    added github oauth

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { auth, signOut, signIn } from '@/auth'; // Ensure these are correctly implemented
+import { redirect } from 'next/dist/server/api-utils';
+
+const Navbar = async () => {
+  let session;
+
+  try {
+    session = await auth(); // Ensure auth() returns a valid session
+  } catch (error) {
+    console.error('Error fetching session:', error);
+  }
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span >Logout</span>
+                  
+                </button>
+              </form>
+
+          <Link href={`/user/${session.id}`}>
+            <span>{session.user.name}</span>
+          </Link>
+        </>
+        ) : (
+        <button onClick={async () => {
+          "use server";
+
+          await signIn('github')
+        }}>
+          <span>Login</span>
+        </button>
+          )}
+      </div>
+    </nav>
+    </header >
+  );
+};
+
+export default Navbar;
