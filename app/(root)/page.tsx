fe8b881d7359e692231aa0adaaa41f6bd9770fa8
gitPirate12 <aneeqshaffy7@gmail.com>
2025-01-01 18:12:50 +0530

    sanity live fetch added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 
 
@@ -10,12 +10,11 @@ export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
- 
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-  const posts = await client.fetch(STARTUPS_QUERY);
 
- 
 
   return (
     <>
@@ -50,6 +49,7 @@ export default async function Home({ searchParams }: {
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
 
   );
